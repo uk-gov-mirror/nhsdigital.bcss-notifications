e5caae00a3113d5415e22fd019230d2a3208ea14
Kieran Harper <k.harper@nhs.net>
2025-12-12 12:25:34 +0000

    Tweaks 2

--- message_status_handler/message_status_recorder.py
+++ message_status_handler/message_status_recorder.py
@@ -33,16 +33,15 @@ def record_message_status(json_data: dict) -> int:
                     f"Message {message_reference} does not exist"
                 )
 
-         #   batch_id = fetch_batch_id_for_message(cursor, message_reference)
-         #   if batch_id is not None:
-            else:
+            # batch_id = fetch_batch_id_for_message(cursor, message_reference)
+            if message_id is not None:
                 response_code = update_message_status(
                     cursor, message_reference
                 )
-          #  else:
-           #     logging.warning(
-            #        "Cannot update status of message %s", message_reference
-            #    )
+            else:
+                logging.warning(
+                    "Cannot update status of message %s", message_reference
+                )
 
     if response_code > 0:
         logging.error(

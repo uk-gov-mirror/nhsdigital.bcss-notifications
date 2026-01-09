599beb95a033922db77505cefec3f3b53933b1a3
Kieran Harper <k.harper@nhs.net>
2026-01-09 10:59:43 +0000

    Tweaks 6

--- shared/recipient.py
+++ shared/recipient.py
@@ -13,3 +13,11 @@ class Recipient(NamedTuple):
     address_line_4: str | None = None
     address_line_5: str | None = None
     postcode: str | None = None
+    sender_org_name: str | None = None
+    sender_org_address_line_1: str | None = None
+    sender_org_address_line_2: str | None = None
+    sender_org_address_line_3: str | None = None
+    sender_org_address_line_4: str | None = None
+    sender_org_address_line_5: str | None = None
+    sender_org_postcode: str | None = None
+    sender_org_email: str | None = None
